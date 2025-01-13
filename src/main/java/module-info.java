73b6df42f2e04bdd0cc5ecde943eec356ba916a4
bytecodeanuj <anujv8692@gmail.com>
2025-01-13 11:14:52 +0530

    Attendance System using RFID

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module com.geekyanuj.projectrfid {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.geekyanuj.projectrfid to javafx.fxml;
+    exports com.geekyanuj.projectrfid;
+    exports com.geekyanuj.projectrfid.controller;
+    opens com.geekyanuj.projectrfid.controller to javafx.fxml;
+}
