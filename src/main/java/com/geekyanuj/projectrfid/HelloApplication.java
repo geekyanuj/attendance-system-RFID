dc84428514a3e6ed794133bd27d8216181ee6b4d
bytecodeanuj <anujv8692@gmail.com>
2025-01-14 14:37:55 +0530

    Added Filter Button

--- src/main/java/com/geekyanuj/projectrfid/HelloApplication.java
+++ src/main/java/com/geekyanuj/projectrfid/HelloApplication.java
@@ -3,6 +3,7 @@ package com.geekyanuj.projectrfid;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,9 +11,14 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        System.out.println(getClass().getResource("Home.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Home.fxml"));
+//        double height=  Screen.getPrimary().getBounds().getHeight();
+//        double width=   Screen.getPrimary().getBounds().getWidth();
+        Scene scene = new Scene(fxmlLoader.load(), 1366, 768);
+
+        stage.setTitle("Attendance System");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
